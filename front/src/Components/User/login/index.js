0f750af8316c50298135a3f425e7ead56fa8f09d
Patrick Gingras <ging0044@algonquinlive.com>
2018-03-31 14:17:15 -0400

    user login should work

--- front/src/Components/User/login/index.js
+++ front/src/Components/User/login/index.js
@@ -3,25 +3,43 @@ import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 class Login extends Component {
-  render() {
-    <Form onSubmit={submit}>
-      <FormGroup>
-        <Label for="email">email</Label>
-        <Input type="email" name="email" id="email" placeholder="email address" />
-      </FormGroup>
+  constructor(props) {
+    super(props);
+    this.state = {
+      username = "",
+      password = "",
+    }
 
-      <FormGroup>
-        <Label for="password">password</Label>
-        <Input type="password" name="password" id="password" placeholder="password" />
-      </FormGroup>
-
-      <FormGroup>
-        <Input type="submit" value="login" />
-      </FormGroup>
-    </Form>
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  submit(e) {
+  handleSubmit(e) {
     e.preventDefault();
+    const result = await lib.bellbois.userLogin(
+      {
+        this.username,
+        this.password,
+      }
+    );
+  }
+
+  render() {
+    return (
+      <Form onSubmit={submit}>
+        <FormGroup>
+          <Label for="email">email</Label>
+          <Input type="email" name="email" id="email" placeholder="email address" />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="password">password</Label>
+          <Input type="password" name="password" id="password" placeholder="password" />
+        </FormGroup>
+
+        <FormGroup>
+          <Input type="submit" value="login" />
+        </FormGroup>
+      </Form>
+    )
   }
 }
