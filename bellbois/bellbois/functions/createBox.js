80f26bc11793e831db5d73115e508f4102115489
Andrew Godfroy <killerrin@users.noreply.github.com>
2018-03-31 18:16:08 -0400

    Added Latitude/Longitude to API

--- bellbois/bellbois/functions/createBox.js
+++ bellbois/bellbois/functions/createBox.js
@@ -8,12 +8,14 @@ const uuidv4 = require("uuid/v4");
  * @param {string} name box's name
  * @param {string} picture picture
  * @param {string} description box's description
+ * @param {double} latitude of the box
+ * @param {double} longitude of the box
  * @returns {object}
  */
-module.exports =  async (userID, name = 'box', picture = null, description = null, context) => {
+module.exports =  async (userID, name = 'box', picture = null, description = null, latitude = null, longitude = null, context) => {
   const id = uuidv4();
 
-  const result = await query("INSERT INTO `Boxes` (ID, name, userID, picture, description) VALUES (?, ?, ?, ?, ?)", [id, name, userID, picture, description]);
+  const result = await query("INSERT INTO `Boxes` (ID, name, userID, picture, description, latitude, longitude) VALUES (?, ?, ?, ?, ?, ?, ?)", [id, name, userID, picture, description, latitude, longitude]);
 
-  return {id, name, userID, picture, description};
+  return {id, name, userID, picture, description, latitude, longitude};
 };
