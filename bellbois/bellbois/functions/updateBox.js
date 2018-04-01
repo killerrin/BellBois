ad333ec557184884faa2072be866207f62dbe55d
Andrew Godfroy <killerrin@users.noreply.github.com>
2018-03-31 20:50:04 -0400

    Revert "Update BoxItems"
    This reverts commit aaf90970ee6a3654a711c20a65a34da2fb6260b9.

--- bellbois/bellbois/functions/updateBox.js
+++ bellbois/bellbois/functions/updateBox.js
@@ -2,13 +2,15 @@ const query = require("../services/SQLService");
 
 /**
  * A basic Hello World function
- * @param {string} ID boxItem id.
- * @param {string} boxID
+ * @param {string} id box id.
  * @param {string} name
- * @param {string} userID
+ * @param {string} picture
+ * @param {string} description
+ * @param {double} latitude of the box
+ * @param {double} longitude of the box
  * @returns {object}
  */
-module.exports =  async (id, boxID, name, userID, context) => {
-  const result = await query("UPDATE `BoxItems` SET boxID = ?, name = ?, userID = ? WHERE `BoxItems`.`id` = ?", [boxID, name, userID, id]);
-  return {id, name, boxID, userID};
+module.exports =  async (id, name = 'box', picture = null, description = null, latitude = null, longitude = null, context) => {
+  const result = await query("UPDATE `Boxes` SET name = ?, picture = ?, description = ?, latitude = ?, longitude = ? WHERE `Boxes`.`id` = ?", [name, picture, description, latitude, longitude, id]);
+  return {id, name, picture, description};
 };
