604d0b79cc6090a65f554dd824320d9e75f6d8f4
bearium <jacob_martin@rogers.com>
2018-03-31 21:56:20 -0400

    fix bugs, added redirects

--- front/src/Components/box/edit/index.js
+++ front/src/Components/box/edit/index.js
@@ -3,6 +3,9 @@ import lib from '../../../lib/lib';
 
 
 import { Form,FormGroup,Label, Input,ListGroup, ListGroupItem } from 'reactstrap';
+import { Redirect } from 'react-router';
+
+
 
 
 class Edit extends Component {
@@ -11,46 +14,70 @@ class Edit extends Component {
     super(props);
     console.log(props);
     this.state = {
+      items: [],
       box: {},
-    }
+      redirect: false,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
 
 
+
+
+
+  handleSubmit(e){
+    e.preventDefault();
+    lib.bellbois.bellbois['@dev'].updateBox(this.state.name, this.state.description,this.state.location);
+    this.setState({redirect: true});
+
+  }
+
+  createItem(item) {
+    return (
+      <ListGroupItem>{item}</ListGroupItem>
+    )
+  }
   async getBox(id) {
     this.setState({ boxes: await lib.bellbois.bellbois['@dev'].getBox(id) });
   }
 
   componentDidMount() {
-    {const {id} = this.props.match.params.id
-      console.log(id)}
-    this.getBox(this.id);
+    const {id} = this.props.match.params.id;
+    this.getBox(id);
+
+  }
+
+  onChange(key, event) {
+    this.setState({ [key]: event.target.value });
   }
 
   render() {
+    if (this.state.redirect) {
+      return <Redirect push to="/box" />;
+    }
     return (
-      <Form>
-        <p></p>
+      <Form method="POST" onSubmit={this.handleSubmit}>
         <FormGroup>
           <Label for="name">name</Label>
-          <Input type="name" name="name" id="name" value= {this.state.box.name} placeholder="name" />
+          <Input type="name" name="name" id="name" value= {this.state.box.name} onChange={ this.onChange.bind(this, "name")} placeholder="Name" />
         </FormGroup>
 
         <FormGroup>
           <Label for="description">Description</Label>
-          <Input type="description" name="description" id="description" value= {this.state.box.description} placeholder="description" />
+          <Input type="description" name="description" id="description" value= {this.state.box.description} onChange={ this.onChange.bind(this, "description")} placeholder="description" />
         </FormGroup>
 
         <FormGroup>
           <Label for="location">Location</Label>
-          <Input type="location" name="password" id="password" value= {this.state.box.location} placeholder="location" />
+          <Input type="location" name="password" id="password" value= {this.state.box.location} onChange={ this.onChange.bind(this, "location")} placeholder="location" />
         </FormGroup>
 
         <FormGroup>
           <Label for="Item">Items</Label>
           <Input type="item" name="item" id="item" placeholder="item" />
         <ListGroup>
-
+          {this.state.items.map(this.createItem)}
         </ListGroup>
         </FormGroup>
 
@@ -58,6 +85,7 @@ class Edit extends Component {
           <Input type="submit" value="Save" />
         </FormGroup>
       </Form>
+
 
     )
   }
